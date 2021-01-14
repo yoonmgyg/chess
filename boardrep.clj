5873d0744eae855194d7eb1f86bd39bce7997df8
yoonmgyg <yoonmgyg@gmail.com>
2021-01-14 04:45:29 +0000

    worked on king moves

--- boardrep.clj
+++ boardrep.clj
@@ -17,15 +17,23 @@
 (defn notation-to-int [notation]
   (if (> (count notation) 2)
     (println notation " is greater than 2 characters.")
-    (exp 2 (+ (char-to-int (char (subs notation 0 1)))
+    (exp 2
+      	   (+ (char-to-int (char (subs notation 0 1)))
       	      (* 8 (int (subs notation 1 2))))))))
 
 (defn bin-to-board [bin]
   (let [board (apply str (reverse bin))]
-  
+
   
 ;converts notation to bitmap
 (defn notation-to-board [& notation]
-  (bin-to-board Integer/toBinaryString (reduce + (map #(notation-to-int %) notation))))
+  (bin-to-board
+    (Integer/toBinaryString
+      (reduce +
+               (map #(notation-to-int %) notation))))))
     
-(defn king-moves [notation])
+(defn king-moves [notation]
+  (let [board-index (index-of(Integer/toBinaryString (notation-to-int notation))))]
+    (
+      (if (< board-index  
+    )))
